d5a8e92122259f84c2492a96a05354a33eb860d9
KJM94 <59479116+KJM94@users.noreply.github.com>
2020-11-23 21:57:14 +0900

    평가
    평가DB구현

---
+++ Professor Evaluation/src/evaluation/EvaluationDAO.java
@@ -0,0 +1,51 @@
+package evaluation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class EvaluationDAO {
+	
+	private Connection conn;
+	private ResultSet rs;
+	
+	public EvaluationDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/ProfessorEvaluation?serverTimezone=Asia/Seoul&useSSL=false";
+			String dbID = "root";
+			String dbPassword = "4651";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int write(EvaluationDTO evaluationDTO) {
+		PreparedStatement pstmt = null;
+		
+		try {
+			String SQL = "INSERT INTO EVALUATION VALUES (NULL, ?, ?, ?, ? ,? ,? ,? ,0);";
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, evaluationDTO.getUserID());
+			pstmt.setString(2, evaluationDTO.getProfessorName());
+			pstmt.setInt(4, evaluationDTO.getLectureYear());
+			pstmt.setString(5, evaluationDTO.getSemesterDivide());
+			pstmt.setString(6, evaluationDTO.getEvaluationContent());
+			pstmt.setString(7, evaluationDTO.getScore());
+			return pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1;
+	}
+
+}
---
+++ Professor Evaluation/src/evaluation/EvaluationDTO.java
@@ -0,0 +1,74 @@
+package evaluation;
+
+public class EvaluationDTO {
+	
+	int evaluationID;
+	String userID;
+	String professorName;
+	int lectureYear;
+	String semesterDivide;
+	String evaluationContent;
+	String Score;
+	int likeCount;
+	public int getEvaluationID() {
+		return evaluationID;
+	}
+	public void setEvaluationID(int evaluationID) {
+		this.evaluationID = evaluationID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getProfessorName() {
+		return professorName;
+	}
+	public void setProfessorName(String professorName) {
+		this.professorName = professorName;
+	}
+	public int getLectureYear() {
+		return lectureYear;
+	}
+	public void setLectureYear(int lectureYear) {
+		this.lectureYear = lectureYear;
+	}
+	public String getSemesterDivide() {
+		return semesterDivide;
+	}
+	public void setSemesterDivide(String semesterDivide) {
+		this.semesterDivide = semesterDivide;
+	}
+	public String getEvaluationContent() {
+		return evaluationContent;
+	}
+	public void setEvaluationContent(String evaluationContent) {
+		this.evaluationContent = evaluationContent;
+	}
+	public String getScore() {
+		return Score;
+	}
+	public void setScore(String score) {
+		Score = score;
+	}
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	public EvaluationDTO(int evaluationID, String userID, String professorName, int lectureYear, String semesterDivide,
+			String evaluationContent, String score, int likeCount) {
+		super();
+		this.evaluationID = evaluationID;
+		this.userID = userID;
+		this.professorName = professorName;
+		this.lectureYear = lectureYear;
+		this.semesterDivide = semesterDivide;
+		this.evaluationContent = evaluationContent;
+		Score = score;
+		this.likeCount = likeCount;
+	}
+
+}
